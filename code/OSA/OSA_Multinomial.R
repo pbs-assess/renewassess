e00fd017e2063bd44bd26376c947c01dea8f08a0
Sean Anderson <sean@seananderson.ca>
2024-03-15 13:45:08 -0700

    Set seed and vectorize runif() for matching

--- code/OSA/OSA_Multinomial.R
+++ code/OSA/OSA_Multinomial.R
@@ -18,7 +18,7 @@ X<-matrix(c(
   4,    2,    3,    5,    3,    1,    0,    1,    0,     2,
   1,    3,    3,    0,    5,    5,    4,    3,    1,     0,
   7,    7,    3,    8,    4,    2,    3,    4,   14,    12
-), nrow=6, ncol=10, byrow=TRUE) 
+), nrow=6, ncol=10, byrow=TRUE)
 
 P<-matrix(c(
   0.32, 0.08, 0.16, 0.24, 0.32, 0.20, 0.20, 0.16, 0.16, 0.16,
@@ -27,23 +27,28 @@ P<-matrix(c(
   0.04, 0.16, 0.16, 0.12, 0.04, 0.08, 0.00, 0.08, 0.04, 0.20,
   0.12, 0.08, 0.12, 0.00, 0.12, 0.12, 0.12, 0.08, 0.04, 0.04,
   0.24, 0.28, 0.12, 0.32, 0.36, 0.20, 0.20, 0.28, 0.52, 0.32
-), nrow=6, ncol=10, byrow=TRUE) 
+), nrow=6, ncol=10, byrow=TRUE)
 
 ## Residuals from package
+set.seed(123)
 res <- resMulti(X,P)
 
 
 ## Each column is one sample from a multinomial distribution:
 ## This is my understanding of OSA for multinomials, based on a
 ## series of binomials and RQR transformation to z.
+
 rqrMulti <- function(X,P){
-  
+
   z <- NULL
   row <- NULL
   col <- NULL
+  px <- NULL
   K <- nrow(X)
   N <- ncol(X)
-  
+  a <- NULL
+  b <- NULL
+
   for( i in 1:N ){
     ## Equation for Multinomial to Binomials in Trijoulet paper:
     for( j in 1:(K-1) ){
@@ -55,13 +60,15 @@ rqrMulti <- function(X,P){
         n <- sum( X[j:K,i] )
       }
       ## Now the RQR classic part for adjusting discrete CDF (Dunn and Smyth 1996):
-      b <- pbinom(X[j,i], size = n, prob = p)  # CDF at x
-      a <- pbinom(X[j,i]-1, size = n, prob = p) ## CDF at x-1
-      z <- c( z, qnorm(runif(1, a, b)) )  ## inverse CDF at unif(a,b) is the quantile.
+      b <- c(b, pbinom(X[j,i], size = n, prob = p))  # CDF at x
+      a <- c(a, pbinom(X[j,i]-1, size = n, prob = p)) ## CDF at x-1
+      # z <- c( z, qnorm(runif(1, a, b)) )  ## inverse CDF at unif(a,b) is the quantile.
       row <- c(row, j)
       col <- c(col, i)
     }
   }
+  set.seed(123)
+  z <- qnorm(runif(n = length(a), min = a, max = b))
   data.frame(row = row, col = col, residual = z)
 }
 
@@ -77,3 +84,4 @@ points(qqnorms$x, qqnorms$y, col = 'red', pch = 16)
 
 ## Nearly the same...
 plot(as.numeric(res), res2$residual)  ## A couple are very different.
+abline(0, 1)
