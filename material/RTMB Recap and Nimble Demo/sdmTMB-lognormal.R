2ce997ecb0e6c53af64d4765ee193bfdfa0c5202
Sean Anderson <sean@seananderson.ca>
2024-09-25 12:41:52 -0700

    Add lognormal note

--- material/RTMB Recap and Nimble Demo/sdmTMB-lognormal.R
+++ material/RTMB Recap and Nimble Demo/sdmTMB-lognormal.R
@@ -24,6 +24,7 @@ mean(sim_dat$observed)
 p <- predict(fit)
 mean(p$est)
 mean(exp(p$est))
+mean(rpois(1e6, 1))
 
 # lognormal:
 sim_dat <- sdmTMB_simulate(
@@ -45,6 +46,13 @@ mean(sim_dat$observed)
 p <- predict(fit)
 mean(p$est)
 mean(exp(p$est))
+mean(rlnorm(1e6, meanlog = 1, sdlog = 0.8))
+mean(rlnorm(1e6, meanlog = 1 - 0.8^2/2, sdlog = 0.8))
+
+# so the simulation/estimation is set up such that
+# the mean of the linear predictor (in log space here)
+# is 1 (the intercept value)
+mean(log(rlnorm(1e6, meanlog = 1, sdlog = 0.8)))
 
 # Generalized gamma:
 # 'gengamma Q' is set at 0.5 here by default
