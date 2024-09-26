cb316045427777d84e66c7a7212d6fb52cfe045a
Sean Anderson <sean@seananderson.ca>
2024-09-26 11:06:23 -0700

    Add to lognormal sdmTMB .R

--- material/RTMB Recap and Nimble Demo/sdmTMB-lognormal.R
+++ material/RTMB Recap and Nimble Demo/sdmTMB-lognormal.R
@@ -20,61 +20,90 @@ sim_dat <- sdmTMB_simulate(
 fit <- sdmTMB(observed ~ 1, family = poisson(), data = sim_dat, spatial = "off")
 
 coef(fit)
+exp(coef(fit))
 mean(sim_dat$observed)
 p <- predict(fit)
 mean(p$est)
 mean(exp(p$est))
 mean(rpois(1e6, 1))
 
-# lognormal:
+# lognormal with mean_adjustment = FALSE
 sim_dat <- sdmTMB_simulate(
   formula = ~ 1,
   data = predictor_dat,
   mesh = mesh,
-  family = lognormal(),
+  family = lognormal(mean_adjust = FALSE),
   range = 0.5,
   phi = 0.8,
   sigma_O = 0,
   seed = 42,
   B = 1
 )
-fit <- sdmTMB(observed ~ 1, family = lognormal(), data = sim_dat, spatial = "off")
+fit <- sdmTMB(observed ~ 1, family = lognormal(mean_adjust = FALSE), data = sim_dat, spatial = "off")
+logLik(fit)
 
 coef(fit)
 exp(get_pars(fit)$ln_phi)
 mean(sim_dat$observed)
 p <- predict(fit)
 mean(p$est)
+
+# these match:
 mean(exp(p$est))
-mean(rlnorm(1e6, meanlog = 1, sdlog = 0.8))
 mean(rlnorm(1e6, meanlog = 1 - 0.8^2/2, sdlog = 0.8))
 
-# so the simulation/estimation is set up such that
-# the mean of the linear predictor (in log space here)
-# is 1 (the intercept value)
-mean(log(rlnorm(1e6, meanlog = 1, sdlog = 0.8)))
+# these match:
+mean(exp(p$est + 0.8^2/2))
+mean(rlnorm(1e6, meanlog = 1, sdlog = 0.8))
 
-# Generalized gamma:
-# 'gengamma Q' is set at 0.5 here by default
-# (a bit heavier tailed than the Gamma (phi = Q)
-# but not as much as the lognormal (Q = 0))
+# redo the fit with mean_adjustment = TRUE
+fit2 <- sdmTMB(observed ~ 1, family = lognormal(mean_adjust = TRUE),
+  data = sim_dat, spatial = "off")
+logLik(fit)
+logLik(fit2)
+
+coef(fit2)
+exp(get_pars(fit2)$ln_phi)
+mean(sim_dat$observed)
+p <- predict(fit2)
+mean(p$est)
+
+# now these match:
+mean(exp(p$est))
+mean(rlnorm(1e6, meanlog = 1, sdlog = 0.8))
+
+# now these match:
+mean(exp(p$est - 0.8^2/2))
+mean(rlnorm(1e6, meanlog = 1 - 0.8^2/2, sdlog = 0.8))
+
+# mean_adjust = TRUE in sim and fit:
+
+# lognormal with mean_adjustment = FALSE
 sim_dat <- sdmTMB_simulate(
   formula = ~ 1,
   data = predictor_dat,
   mesh = mesh,
-  family = gengamma(),
+  family = lognormal(mean_adjust = TRUE),
   range = 0.5,
   phi = 0.8,
   sigma_O = 0,
   seed = 42,
   B = 1
 )
-fit <- sdmTMB(observed ~ 1, family = gengamma(), data = sim_dat, spatial = "off")
+fit3 <- sdmTMB(observed ~ 1, family = lognormal(mean_adjust = TRUE), data = sim_dat, spatial = "off")
+logLik(fit3)
 
-coef(fit)
-exp(get_pars(fit)$ln_phi)
-get_pars(fit)$gengamma_Q
+coef(fit3)
+exp(get_pars(fit3)$ln_phi)
 mean(sim_dat$observed)
-p <- predict(fit)
+p <- predict(fit3)
 mean(p$est)
+
+# these match:
 mean(exp(p$est))
+mean(rlnorm(1e6, meanlog = 1 - 0.8^2/2, sdlog = 0.8))
+
+# these match:
+mean(exp(p$est + 0.8^2/2))
+mean(rlnorm(1e6, meanlog = 1, sdlog = 0.8))
+
